41bfea6f73f34429e8c10d38da7514833a927ae2
Helio Silveira <heliomsf@gmail.com>
2023-05-18 18:46:39 +0200

    config buildings controller

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,2 +1,11 @@
 class BuildingsController < ApplicationController
+    # action to retrieve all buildings from the database and render them in a view
+    def index
+        @buildings = Building.all
+    end
+
+    # action to retrieve a single building from the database and render it in a view
+    def show
+        @building = Building.find(params[:id])
+    end
 end
